bf0a851e8c5ef9efa4ec1c17f11b17fd2ffe5629
Jack <worn3895@gmail.com>
2024-07-19 13:44:49 +0900

    연결

--- Assets/Script/MoveScene.cs
+++ Assets/Script/MoveScene.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MoveScene : MonoBehaviour
 {
+    [SerializeField]
+    private GameData gameData;
+
     public void SceneMove(string SceneName)
     {
         SceneManager.LoadScene(SceneName);    
+    }
+
+    public void SceneMoveById(int id)
+    {
+        SceneManager.LoadScene(gameData.stages[id].sceneName);
     }
 }
