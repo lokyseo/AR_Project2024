f63196619b4ad1cf82a537b501b860b9be7bf883
lokyseo <lokyseo0434@nate.com>
2024-08-05 17:00:47 +0900

    18번째 미니게임 완성

--- Assets/Script/MiniGame/CorrectBlock/CorrectBlock_Manager.cs
+++ Assets/Script/MiniGame/CorrectBlock/CorrectBlock_Manager.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class CorrectBlock_Manager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     GameObject clicked_Object;
-
+    public GameObject totalGround_Object;
     Vector3 startPos;
+
+    public int countBool;
+
+    Vector3 lastMousePosition;
+
 
     void Start()
     {
@@ -27,7 +33,13 @@ public class CorrectBlock_Manager : MonoBehaviour, IPointerDownHandler, IPointer
         if (clicked_Object.tag != "Ground")
         {
             startPos = clicked_Object.transform.position;
-            Debug.Log(clicked_Object.name);
+            for (int i = 0; i < clicked_Object.transform.childCount; i++)
+            {
+                clicked_Object.transform.GetChild(i).GetComponent<Image>().raycastTarget = false;
+
+            }
+
+            clicked_Object.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, startPos.z));
         }
         else
         {
@@ -37,19 +49,82 @@ public class CorrectBlock_Manager : MonoBehaviour, IPointerDownHandler, IPointer
 
     public void OnDrag(PointerEventData eventData)
     {
-        if(clicked_Object != null)
+        if (clicked_Object != null)
         {
+            //Vector3 currentMousePosition = Input.mousePosition;
+            //Vector3 mouseDelta = currentMousePosition - lastMousePosition;
+            //clicked_Object.GetComponent<RectTransform>().anchoredPosition += new Vector2(mouseDelta.x, mouseDelta.y);
+            //lastMousePosition = currentMousePosition;
             clicked_Object.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, startPos.z));
-
         }
     }
-
+    
     public void OnPointerUp(PointerEventData eventData)
     {
         if (clicked_Object != null)
         {
+            GameObject upPoint_Object = eventData.pointerCurrentRaycast.gameObject;
+            if (upPoint_Object != null)
+            {
+                if (upPoint_Object.tag == "Ground")
+                {
+                    bool[] isOverLapping = new bool[clicked_Object.transform.childCount];
+                    for (int i = 0; i < clicked_Object.transform.GetComponentsInChildren<BoxCollider2D>().Length; i++)
+                    {
+                        isOverLapping[i] = false;
+                        Collider2D[] colliders = Physics2D.OverlapBoxAll(clicked_Object.transform.GetChild(i).position,
+                            new Vector2(0.5f,0.5f), 0f);
+
+                        foreach (Collider2D collider in colliders)
+                        {
+                            if(collider.gameObject != clicked_Object.transform.GetChild(i).gameObject)
+                            {
+                                Debug.Log(collider.gameObject.name);
+
+                                if (collider.gameObject.tag == "Ground")
+                                {
+                                    isOverLapping[i] = true;
+                                }
+                                if (collider.gameObject.tag == "Block")
+                                {
+                                    Debug.Log("ÀÚ¸®¿ä");
+                                    isOverLapping[i] = false;
+                                    break;
+                                }
+                            }
+                          
+                        }
+                    }
+
+                    countBool = 0;
+                    for (int i = 0; i < clicked_Object.transform.GetComponentsInChildren<BoxCollider2D>().Length; i++)
+                    {
+                        if (!isOverLapping[i])
+                        {
+                            clicked_Object.transform.position = startPos;
+                            break;
+                        }
+                        else
+                        {
+                            countBool++;
+                        }
+                    }
+                    if(countBool == clicked_Object.transform.GetComponentsInChildren<BoxCollider2D>().Length)
+                    {
+                        clicked_Object.transform.position = upPoint_Object.transform.position;
+                    }
+
+                }
+                else if(upPoint_Object.tag == "Block")
+                {
+                    clicked_Object.transform.position = startPos;
+                }
+            }
+            for (int i = 0; i < clicked_Object.transform.childCount; i++)
+            {
+                clicked_Object.GetComponentsInChildren<Image>()[i].raycastTarget = true;
+            }
            
         }
     }
-    
 }
--- Assets/Script/MiniGame/CorrectBlock/OverlapBlock.cs
+++ Assets/Script/MiniGame/CorrectBlock/OverlapBlock.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OverlapBlock : MonoBehaviour
 {
-
+    public bool isFool;
+    //public bool isReadyEquip;
+    private void Start()
+    {
+        isFool = false;
+        //isReadyEquip = false;
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        GetComponent<SpriteRenderer>().color = Color.red;
+        if ((collision.tag == "Block"))
+        {
+            GetComponent<Image>().color = Color.red;
+           
+                isFool = true;
+          
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if ((collision.tag == "Block"))
+        {
+            GetComponent<Image>().color = Color.black;
+            isFool = false;
+        }
     }
 }
